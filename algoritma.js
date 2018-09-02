7f49176df26306af64a31a1c4fcf8b558f28a06a
Hadi NW <hadinw@Hadis-MacBook-Pro.local>
2018-09-02 07:05:23 +0700

    update

---
+++ algoritma.js
@@ -0,0 +1,32 @@
+// // ALGORITMA ATAU PSEUDO CODE
+// // 1. Agar orang lain ngerti kode yang dimaksud
+// // 2. Rancang dulu baru coding
+
+// // PSEUDO CODE MENNGHITUNG LUAS SEGITIGA
+
+// // OUTPUT = Luas Segitiga
+// // Input = Alas, Tinggi
+// // Proses = menghitung alas kali tinggi dinbagi 2
+
+
+// STORE 'alas' with value 5
+// STORE 'tinggi' with value 8
+// CALCULATE 'alas' times 'tinggi'  Divided by 2
+// SET 'Luas Segitiga' with calculation result
+// DISPLAY 'Luas Segitiga'
+
+// // ALGORITMA MENGHITUNG LUAS SEGITIGA
+// 1. Definisikan alas dengan nilai 5
+// 2. Definisikan tinggi dengan nilai 8
+// 3. Hitung luas segitiga dengan alas kali tinggi bagi 2
+// 4. tampilkan luas segitiga
+
+
+// tipe data = number, string, boolean, null , undefined
+
+var alas = 8;
+var tinggi = 6;
+var luas = alas * tinggi / 2;
+
+console.log(luas);
+
